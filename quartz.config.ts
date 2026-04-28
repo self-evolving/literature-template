a78d8f486c2c1ce1de3d7e90633b7807eae8de04
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2026-04-28 01:21:43 -0400

    Update base URL for repo-docs

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "augmented-mind.github.io",
+    baseUrl: "self-evolving.github.io/repo-docs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
